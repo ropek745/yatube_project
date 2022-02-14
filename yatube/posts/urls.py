e03053c172304d96904c4ce46b5c93797ea32a32
Роман Пекарев <hawkdanilow@yandex.ru>
2022-02-14 17:59:34 +0300

    Добавлены namespace и name, сслыка в шапке кликабельна, и ведёт на главную страницу

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('group/', views.group_posts_list),
-    path('group/<slug:slug>/', views.group_posts)
+    path('', views.index, name='posts_global'),
+    path('group/', views.group_posts_list, name='posts_list'),
+    path('group/<slug:slug>/', views.group_posts, name='posts_slug')
 ]  
