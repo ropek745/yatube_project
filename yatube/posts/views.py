9d0bd80d40e193a76b6f38107293dced81128f63
Роман Пекарев <hawkdanilow@yandex.ru>
2022-02-14 16:40:32 +0300

    Добавлен первый шаблон, введен во view-функцию

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 
 # Главная страница
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render (request, template)
     
 
 # Страница со списком групп
